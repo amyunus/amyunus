55116643ecad8548c9832d5caaf72f1dd08d7283
AMYunus <ahmad.mahmudi.yunus@gmail.com>
2012-10-06 09:46:58 +0700

    Ready to ship into v1.1
    Screenshot added
    Readme updated
    Minor style to widget added
    Minor style to archive page
    Minor bugfixes to above menu

--- footer.php
+++ footer.php
@@ -16,7 +16,7 @@
 			<?php do_action( 'amyunus_credits' ); ?>
 			<a href="//wordpress.org/" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'amyunus' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', 'amyunus' ), 'WordPress', '<a href="//www.amyunus.com/" rel="author">AMYunus</a>' ); ?></a>
 			<span class="sep"> | </span>
-			<a href="//www.amyunus.com/" title="<?php esc_attr_e( 'Theme styler', 'amyunus' ); ?>" rel="generator"><?php printf( __( 'Theme styled by %s', 'amyunus' ), 'AMYunus' ); ?></a>
+			<a href="//www.amyunus.com/themes/amyunus-wordpress-theme/" title="<?php esc_attr_e( 'Theme styler', 'amyunus' ); ?>" rel="generator"><?php printf( __( 'Theme styled by %s', 'amyunus' ), 'AMYunus' ); ?></a>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon .site-footer -->
 </div><!-- #page .hfeed .site -->
